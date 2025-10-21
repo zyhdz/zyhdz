81a518899fe485a7aacd2ae9cd8ba4d356cd9090
zyhdz <805971612@qq.com>
2025-10-21 17:09:35 +0800

    2_main.c

---
+++ 2_main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int score;
+    scanf("%d", &score);
+    if (score >= 90 && score <= 100) {
+        printf("A\n");
+    } else if (score >= 80 && score <= 89) {
+        printf("B\n");
+    } else if (score >= 70 && score <= 79) {
+        printf("C\n");
+    } else if (score >= 60 && score <= 69) {
+        printf("D\n");
+    } else if (score >= 0 && score < 60) {
+        printf("E\n");
+    } else {
+        printf("输入成绩无效\n");
+    }
+    return 0;
+}
