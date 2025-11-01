058b64eb5bf68add28ca5e60e2208a5a9baeef72
zyhdz <805971612@qq.com>
2025-11-01 14:07:35 +0800

    Update 3_main.c

--- 3_main.c
+++ 3_main.c
@@ -2,31 +2,36 @@
 //805971612@qq.com
 //周玉恒
 #include <stdio.h>
+#include <math.h>
 
 int main() {
-    int a, b;
-    char op;
-    scanf("%d %d %c", &a, &b, &op);
-    switch (op) {
-        case '+':
-            printf("%d\n", a + b);
-            break;
-        case '-':
-            printf("%d\n", a - b);
-            break;
-        case '*':
-            printf("%d\n", a * b);
-            break;
-        case '/':
-            if (b != 0) {
-                printf("%d\n", a / b); 
-            } else {
-                printf("除数不能为 0\n");
+    int n;
+   
+    scanf("%d", &n);
+    
+    int is_prime = 1; 
+    int i = 2;         
+    
+   
+    if (n <= 1) {
+        is_prime = 0;
+    } else {
+       
+        while (i <= sqrt(n)) {
+            if (n % i == 0) {
+                is_prime = 0;  
+                break;        
             }
-            break;
-        default:
-            printf("输入运算符无效\n");
-            break;
+            i++;  
+        }
     }
+    
+   
+    if (is_prime) {
+        printf("密钥安全，密码设置成功\n");
+    } else {
+        printf("密钥不安全，请重新输入\n");
+    }
+    
     return 0;
 }
