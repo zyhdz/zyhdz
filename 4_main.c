638fd8750c3c05da43e8d11e49e2b14ec96d2146
zyhdz <805971612@qq.com>
2025-11-01 14:09:49 +0800

    Update 4_main.c

--- 4_main.c
+++ 4_main.c
@@ -4,13 +4,30 @@
 #include <stdio.h>
 
 int main() {
-    int a, b, c;
-    printf("请输入三条线段的长度（以空格分隔）：");
-    scanf("%d %d %d", &a, &b, &c);
-    if ((a + b > c) && (a + c > b) && (b + c > a)) {
-        printf("可以组成三角形\n");
-    } else {
-        printf("不能组成三角形\n");
+    int num = 100;  
+    int count = 0;  
+    int hundreds, tens, units;
+
+   
+    while (num <= 999) {
+      
+        hundreds = num / 100;
+        tens = (num / 10) % 10;
+        units = num % 10;
+
+        
+        if (hundreds * hundreds * hundreds + tens * tens * tens + units * units * units == num) {
+            
+            if (count > 0) {
+                printf(" ");
+            }
+            printf("%d", num);
+            count++;
+        }
+
+        num++; 
     }
-    
+    printf("\n"); 
+
     return 0;
+}
