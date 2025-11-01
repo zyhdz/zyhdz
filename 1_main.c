12f5312ea8ef222d2471555118c576c7aa42f865
zyhdz <805971612@qq.com>
2025-11-01 13:55:49 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -2,10 +2,34 @@
 //805971612@qq.com
 //周玉恒
 #include <stdio.h>
+#include <math.h>  
+
+
+int isPrime(int num) {
+    
+    if (num <= 1) {
+        return 0;
+    }
+    
+    for (int i = 2; i <= sqrt(num); i++) {
+        if (num % i == 0) {
+            return 0;  
+        }
+    }
+    return 1; 
+}
+
 int main() {
-    float celsius, fahrenheit;
-    scanf("%f", &celsius);
-    fahrenheit = celsius * 9.0 / 5.0 + 32;
-    printf("%.1f\n", fahrenheit);
+    int n;
+   
+    scanf("%d", &n);
+    
+    
+    if (isPrime(n)) {
+        printf("密钥安全，密码设置成功\n");
+    } else {
+        printf("密钥不安全，请重新输入\n");
+    }
+    
     return 0;
 }
